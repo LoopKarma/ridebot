e107b4decf1a0461149ab3171a375f8c9e67290d
Evgenii Molov <evgenii.molov@auto1.com>
2017-04-25 22:56:30 +0200

    changes of main logic. +timeHelper

--- ridebot.go
+++ ridebot.go
@@ -8,90 +8,104 @@ import (
 	"encoding/json"
 	"net/url"
 	"time"
+	"gopkg.in/telegram-bot-api.v4"
 )
 
 var targetPoint = Coordinates{
-	Lat:52.4833,
-	Lon:13.3833,
+	Lat:52.498126,
+	Lon:13.399683,
 }
 var apiHost = "http://api.openweathermap.org/data/2.5/"
 var apiKey = "b2896361aa576c90279d9f966b80aa8d"
 
 
 func main() {
+	//bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
 	var ride = RideParams{
-		duration:4,
-		isNightRide:true,
-		minTemp:-5,
+		duration:5,
+		isNightRide:false,
+		minTemp:5,
 		isRainRide:true,
+		location:targetPoint,
+		//debug: true,
 	}
+	fmt.Printf("Your ride params:\n%+v\n", ride)
+	fmt.Println("**************************************************")
 
-	forecast := getImportantForecast(ride)
+	forecast := getForecastForRide(ride)
 	if (len(forecast.List) > 0) {
-		message := "\n\n" + forecast.City.Name + ": \n";
-		for _,point := range forecast.List {
-			message += "In " + point.DtTxt + " min = " + floatToString(point.Main.TempMin, 4) + "˚; max = " + floatToString(point.Main.TempMax) + "˚\n"
-			message += point.Weather[0].Description + "\n"
-		}
-		fmt.Println("\n\n\n")
-		fmt.Println(message)
-		fmt.Println("\n\n\n")
+		fmt.Println(formatRideForecast(forecast))
+	} else {
+		fmt.Println("No forecast for your ride")
 	}
 }
 
-func getImportantForecast(ride RideParams) ForecastResponse {
-	weather := getCurrentWeather()
-	forecast := getForecast()
+func formatRideForecast(forecast ForecastResponse) string {
+	headerFormat := "\n%s:\n"
+	message := fmt.Sprintf(headerFormat, forecast.City.Name )
+	for _,point := range forecast.List {
+		pointFormat := "%s [min = %.2f˚; max = %.2f˚]\nDescription: %s. Result: %t\n"
+		message += fmt.Sprintf(
+			pointFormat,
+			point.DtTxt,
+			point.Main.TempMin,
+			point.Main.TempMax,
+			point.Weather[0].Description,
+			point.isGoodConditions,
+		)
+	}
+	return message
+}
 
-
+func getForecastForRide(ride RideParams) ForecastResponse {
+	weather := getCurrentWeather(ride.location.Lat, ride.location.Lon)
+	forecast := getForecast(ride.location.Lat, ride.location.Lon)
 
 	var importantPoints []List
+	var previousPoint List
+	var timeBetweenTwoPoints int
+	timestamp := int(time.Now().UTC().Unix())
+	rideEnd := timestamp + int((ride.duration * 3600))
+	sunset := weather.Sys.Sunset
 
 	for _, point := range forecast.List {
-		if isImportantPoint(weather, point, ride) {
+		var add = false
+
+		if (ride.debug) {
+			fmt.Println("\n\ntimestamp point.Dt = ",point.Dt)
+			fmt.Println("timestamp rideEnd = ",rideEnd)
+			fmt.Println("Date point.Dt ",point.DtTxt)
+			fmt.Println("Date rideEnd ", timestampToTimeUTC(rideEnd))
+			fmt.Println("point.Dt < rideEnd", point.Dt < rideEnd)
+			fmt.Println("point.Dt - rideEnd", point.Dt - rideEnd)
+		}
+
+		if ride.isNightRide == false && point.Dt > sunset {//point in the evening
+			continue
+		}
+		if timestamp > point.Dt {//point in the past
+			add = true
+		}
+		if point.Dt < rideEnd {//point before ride ending
+			add = true
+		}
+		if &previousPoint != nil {
+			timeBetweenTwoPoints = point.Dt - previousPoint.Dt
+		}
+		if &timeBetweenTwoPoints != nil  && (point.Dt - rideEnd) < timeBetweenTwoPoints / 2 {
+			add = true
+		}
+		if add {
+			point.isGoodConditions = isGoodConditions(ride, point)
 			importantPoints = append(importantPoints, point)
 		}
+		previousPoint = point
 	}
 	forecast.List = importantPoints
-
 
 	forecast.Sys = weather.Sys
 
 	return forecast
-}
-
-func isImportantPoint(currentWeather WeatherResponse, point List, ride RideParams) bool {
-	sunset := currentWeather.Sys.Sunset
-	rideEnd := currentWeather.Dt + (ride.duration * 3600)
-
-	//fmt.Println("\n sunset ", timestampToHumanType(sunset))
-	//fmt.Println("human time of point ", point.DtTxt)
-
-	if ride.isNightRide == false && point.Dt > sunset { //point in the evening
-		//fmt.Println("point.Dt > sunset ", point.Dt > sunset)
-		return false
-	}
-
-
-	//fmt.Println("\n curtime ", timestampToHumanType(currentWeather.Dt))
-	//fmt.Println("currentWeather.Dt > point.Dt ", currentWeather.Dt > point.Dt)
-
-	if currentWeather.Dt > point.Dt {     //point in the past
-		return isGoodConditions(ride, point)
-	}
-
-	fmt.Println("\n point.Dt ",point.DtTxt)
-	fmt.Println("\n point.Dt ",point.Dt)
-	fmt.Println("rideEnd ",timestampToHumanType(rideEnd))
-	fmt.Println("rideEnd ",rideEnd)
-	fmt.Println("point.Dt < rideEnd", point.Dt < rideEnd)
-
-	if point.Dt < rideEnd {
-		return isGoodConditions(ride, point)
-	}
-
-
-	return false
 }
 
 func isGoodConditions(ride RideParams, point List) bool {
@@ -115,10 +129,10 @@ func isGoodConditions(ride RideParams, point List) bool {
 }
 
 
-func getForecast() ForecastResponse {
+func getForecast(latitude float64, longitude float64) ForecastResponse {
 	requestParams := map[string]string{
-		"lat": floatToString(targetPoint.Lat),
-		"lon":   floatToString(targetPoint.Lon),
+		"lat": floatToString(latitude),
+		"lon":   floatToString(longitude),
 	}
 
 	resp, err := http.Get(createApiUri("forecast", requestParams))
@@ -141,19 +155,13 @@ func getForecast() ForecastResponse {
 		panic(err)
 	}
 
-	//TODO consider to remove debug messages
-	//stringContent := string(content)
-	//fmt.Println("\n\n\n")
-	//fmt.Println(stringContent)
-	//fmt.Println("\n\n\n")
-
 	return result
 }
 
-func getCurrentWeather() WeatherResponse {
+func getCurrentWeather(latitude float64, longitude float64) WeatherResponse {
 	requestParams := map[string]string{
-		"lat": floatToString(targetPoint.Lat),
-		"lon":   floatToString(targetPoint.Lon),
+		"lat": floatToString(latitude),
+		"lon":   floatToString(longitude),
 	}
 
 	resp, err := http.Get(createApiUri("weather", requestParams))
@@ -176,12 +184,6 @@ func getCurrentWeather() WeatherResponse {
 		panic(err)
 	}
 
-	//TODO consider to remove debug messages
-	//stringContent := string(content)
-	//fmt.Println("\n\n\n")
-	//fmt.Println(stringContent)
-	//fmt.Println("\n\n\n")
-
 	return result
 }
 
@@ -195,8 +197,7 @@ func createApiUri(method string, parameters map[string]string) string {
 	values.Add("lang", "ru")
 
 	uri := apiHost + method + "?" + values.Encode()
-	fmt.Println(uri)
-
+	//fmt.Println(uri)
 	return uri
 }
 
@@ -210,8 +211,8 @@ func floatToString(input_num float64, precision ...int) string {
 	return strconv.FormatFloat(input_num, 'f', prec, 64)
 }
 
-func timestampToHumanType(timestamp int) string {
-	return time.Unix(int64(timestamp), 0).Format("Mon Jan 2 15:04:05 -0700 MST 2006")
+func timestampToTimeUTC(timestamp int) string {
+	return time.Unix(int64(timestamp), 0).UTC().Format("Mon Jan 2 15:04:05 -0700 MST 2006")
 }
 
 
@@ -266,6 +267,7 @@ type WeatherResponse struct {
 }
 
 type List struct {
+	isGoodConditions bool
 	Dt int `json:"dt"`
 	Main struct {
 		   Temp float64 `json:"temp"`
@@ -312,8 +314,10 @@ type Sys struct {
 }
 
 type RideParams struct {
-	duration    int
+	duration    float64
 	isNightRide bool
 	minTemp     int
 	isRainRide  bool
+	location    Coordinates
+	debug       bool
 }
--- timeHelper.go
+++ timeHelper.go
@@ -1,28 +1,65 @@
 package main
 
 import (
-	"time"
-	"net/http"
-	"net/url"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
-var apiHost = "https://maps.googleapis.com/"
-var apiMethod = "maps/api/timezone/json"
+const googleURI = "https://maps.googleapis.com/maps/api/timezone/json?location=%f,%f&timestamp=%d&sensor=false"
 
-func getLocalTimeByCoordinates(coord Coordinates) time.Time {
-	values := url.Values{}
-	parameters := map[string]string {
-		"location": string(coord.Lat)+","+string(coord.Lon),
-		"timestamp": string(time.Now().Unix()),
+func RetrieveGoogleTimezone(latitude float64, longitude float64) (googleTimezone *GoogleTimezone, err error) {
+	//defer func() {
+	//	if r := recover(); r != nil {
+	//		err = fmt.Errorf("%v", r)
+	//	}
+	//}()
+	timestamp := time.Now().UTC().Unix()
+
+	uri := fmt.Sprintf(googleURI, latitude, longitude, timestamp)
+
+	resp, err := http.Get(uri)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return googleTimezone, err
 	}
-	for key, value := range parameters {
-		values.Add(key, value)
+
+
+
+	// Convert the response to a byte array
+	rawDocument, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return googleTimezone, err
 	}
-	requestUrl = apiHost + apiMethod + "?" + values.Encode()
 
+	// Unmarshal the response to a GoogleTimezone object
+	googleTimezone = new(GoogleTimezone)
+	if err = json.Unmarshal(rawDocument, googleTimezone); err != nil {
+		return googleTimezone, err
+	}
 
+	if googleTimezone.Status != "OK" {
+		err = fmt.Errorf("Error : Google Status : %s", googleTimezone.Status)
+		return googleTimezone, err
+	}
+
+	//if len(GoogleTimezone.TimezoneID) == 0 {
+	//	err = fmt.Errorf("Error : No Timezone Id Provided")
+	//	return googleTimezone, err
+	//}
+	googleTimezone.Timestamp = timestamp + int64(googleTimezone.RawOffset)
+
+	return googleTimezone, err
 }
 
-
-
+type GoogleTimezone struct {
+	DstOffset    float64 `json:"dstOffset"`
+	RawOffset    float64 `json:"rawOffset"`
+	Status       string  `json:"status"`
+	TimezoneID   string  `json:"timeZoneId"`
+	TimezoneName string  `json:"timeZoneName"`
+	Timestamp    int64
+}
